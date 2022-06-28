3bc1e8b6e731e13f4944cc9d7c9c93aa7b1f8e7f
jemis-maru <jemis.maru@bacancy.com>
2022-06-28 11:43:56 +0530

    Some UI changes

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "process.env.VUE_APP_BASE_URL",
+  baseURL: "https://bkart-ecommerce0.herokuapp.com/",
 });
 
 axiosInstance.interceptors.request.use(
